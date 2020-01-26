612ee06d401c4e4ee429e781c24053a72f3e7091
Chug D <44780869+dchug2@users.noreply.github.com>
2020-01-26 13:05:30 -0600

    Create remove_later.py
    This is a test file. This will be removed before the final pull request.

---
+++ ATMS-597-SP-2020-Project-1/remove_later.py
@@ -0,0 +1,2 @@
+#This file is to test my integration of github with google collab. The idea is to pull recent changes in github repo into google drive, and work there to develop code.
+print('Hello world!')
