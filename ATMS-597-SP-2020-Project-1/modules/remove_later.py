721b285f23794ba8b890ab386a7ca9877549bd24
Chug D <44780869+dchug2@users.noreply.github.com>
2020-01-27 10:04:11 -0600

    Delete remove_later.py
    This code was only intended to test google drive set up with github

--- ATMS-597-SP-2020-Project-1/modules/remove_later.py
+++
@@ -1,2 +0,0 @@
-#This file is to test my integration of github with google collab. The idea is to pull recent changes in github repo into google drive, and work there to develop code.
-print('Hello world!')
