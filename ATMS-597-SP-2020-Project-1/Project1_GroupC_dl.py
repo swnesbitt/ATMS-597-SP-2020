6dc365438871b91e2f16e52572d9d30b52b806fe
David Lafferty <david.lafferty94@gmail.com>
2020-01-29 13:43:19 -0600

    Updated David's script

--- ATMS-597-SP-2020-Project-1/Project1_GroupC_dl.py
+++ ATMS-597-SP-2020-Project-1/Project1_GroupC_dl.py
@@ -1,4 +1,3 @@
-import sys
 import numpy as np
 
 class TempConvert:
@@ -10,66 +9,51 @@ class TempConvert:
 
     def toF(self):
         if self.unit == "C":
-            res = self.value * 9/5 + 32
+            self.value = self.value * 9/5 + 32
         elif self.unit == "K":
-            res = self.value * 9/5 - 459.67
+            self.value = self.value * 9/5 - 459.67
         elif self.unit == "F":
-            res = self.value * 1
-
-        if self.type == list:
-            return np.round(res, 2).tolist()
-        else:
-            return np.round(res, 2)
+            self.value = self.value * 1
 
     def toK(self):
         if self.unit == "C":
-            res = self.value + 273.15
+            self.value = self.value + 273.15
         elif self.unit == "F":
-            res = (self.value + 459.67) * 5/9
+            self.value = (self.value + 459.67) * 5/9
         elif self.unit == "K":
-            res = self.value * 1
-
-        if self.type == list:
-            return np.round(res, 2).tolist()
-        else:
-            return np.round(res, 2)
+            self.value = self.value * 1
 
     def toC(self):
         if self.unit == "K":
-            res = self.value - 273.15
+            self.value = self.value - 273.15
         elif self.unit == "F":
-            res = (self.value - 32) * 5/9
+            self.value = (self.value - 32) * 5/9
         elif self.unit == "C":
-            res = self.value * 1
+            self.value = self.value * 1
 
-        if self.type == list:
-            return np.round(res, 2).tolist()
-        else:
-            return np.round(res, 2)
+# # Input values as list
+# temps_list = [10, -50.5, 123, -0.1]
+# unit_list = "C"
 
-# Input values as list
-temps_list = [10, -50.5, 123, -0.1]
-unit_list = "C"
+# print("Input values selected as data type: " + str(type(temps_list)))
+# print(str(temps_list) + " " + unit_list)
 
-print("Input values selected as data type: " + str(type(temps_list)))
-print(str(temps_list) + " " + unit_list)
-
-print("\nConverting now... Results:")
-temps = TempConvert(temps_list, unit_list)
-print(str(temps.toC()) + " " + "C")
-print(str(temps.toF()) + " " + "F")
-print(str(temps.toK()) + " " + "K\n\n")
+# print("\nConverting now... Results:")
+# temps = TempConvert(temps_list, unit_list)
+# print(str(temps.toC()) + " " + "C")
+# print(str(temps.toF()) + " " + "F")
+# print(str(temps.toK()) + " " + "K\n\n")
 
 
-# Input values as numpy array
-temps_np = np.array([-18.5, 0.0, 29, 100])
-unit_np = "F"
+# # Input values as numpy array
+# temps_np = np.array([-18.5, 0.0, 29, 100])
+# unit_np = "F"
 
-print("Input values selected as data type: " + str(type(temps_np)))
-print(str(temps_np) + " " + unit_np)
+# print("Input values selected as data type: " + str(type(temps_np)))
+# print(str(temps_np) + " " + unit_np)
 
-print("\nConverting now... Results:")
-temps = TempConvert(temps_list, unit_np)
-print(str(temps.toC()) + " " + "C")
-print(str(temps.toF()) + " " + "F")
-print(str(temps.toK()) + " " + "K")
+# print("\nConverting now... Results:")
+# temps = TempConvert(temps_list, unit_np)
+# print(str(temps.toC()) + " " + "C")
+# print(str(temps.toF()) + " " + "F")
+# print(str(temps.toK()) + " " + "K")
