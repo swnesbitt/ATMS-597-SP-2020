5c01cf25c5c325b34eca3dc726466aa710b810b7
michaelsessa <31252875+michaelsessa@users.noreply.github.com>
2020-01-30 18:06:29 -0600

    Update to PEP8 spacing requirements

--- ATMS-597-SP-2020-Project-1/atms597_project1_groupd.py
+++ ATMS-597-SP-2020-Project-1/atms597_project1_groupd.py
@@ -12,44 +12,44 @@ class tempconvert_class:
     # Convert a user provided temperature from Celcius to Fahrenheit.
     def C2F(self, temp_c):
         if type(temp_c) == list:
-            return ([item*1.8+32 for item in temp_c])
+            return ([item * 1.8 + 32 for item in temp_c])
         else:
-            return temp_c*1.8+32
+            return temp_c * 1.8 + 32
 
     # Convert a user provided temperature from Celcius to Kelvin.
     def C2K(self, temp_c):
         if type(temp_c) == list:
-            return ([item+273.15 for item in temp_c])
+            return ([item + 273.15 for item in temp_c])
         else:
-            return temp_c+273.15
+            return temp_c + 273.15
 
     # Convert a user provided temperature from Fahrenheit to Celcius.
     def F2C(self, temp_f):
         if type(temp_f) == list:
-            return ([(item-32)/1.8 for item in temp_f])
+            return ([(item - 32) / 1.8 for item in temp_f])
         else:
-            return (temp_f-32)/1.8
+            return (temp_f - 32) / 1.8
 
     # Convert a user provided temperature from Fahrenheit to Kelvin.
     def F2K(self, temp_f):
         if type(temp_f) == list:
-            return ([(item-32)/1.8+273.15 for item in temp_f])
+            return ([(item - 32)/1.8 + 273.15 for item in temp_f])
         else:
-            return (temp_f-32)/1.8+273.15
+            return (temp_f - 32)/1.8 + 273.15
 
     # Convert a user provided temperature from Kelvin to Celcius.
     def K2C(self, temp_k):
         if type(temp_k) == list:
-            return ([item-273.15 for item in temp_k])
+            return ([item - 273.15 for item in temp_k])
         else:
-            return temp_k-273.15
+            return temp_k - 273.15
 
     # Convert a user provided temperature from Kelvin to Fahrenheit.
     def K2F(self, temp_k):
         if type(temp_k) == list:
-            return ([(item-273.15)*1.8+32 for item in temp_k])
+            return ([(item - 273.15) * 1.8 + 32 for item in temp_k])
         else:
-            return (temp_k-273.15)*1.8+32
+            return (temp_k - 273.15) * 1.8 + 32
 
 tempconvert = tempconvert_class()  # Initialized instance
 tempconvert.C2F  # bound method
