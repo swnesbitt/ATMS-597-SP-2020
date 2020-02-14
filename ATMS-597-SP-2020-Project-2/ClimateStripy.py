65720b4f9500bb139fcdfbe6d28b9e087303f4cb
Randy J. Chase <randy.chase12@gmail.com>
2020-02-13 18:06:18 -0600

    final push for submission

--- ATMS-597-SP-2020-Project-2/ClimateStripy.py
+++ ATMS-597-SP-2020-Project-2/ClimateStripy.py
@@ -5,6 +5,24 @@ import tqdm
 import numpy as np
 import matplotlib.pylab as plt
 
+# move to beginning when merging
+import datetime
+import matplotlib.dates as mdates
+from matplotlib.patches import Rectangle
+from matplotlib.collections import PatchCollection
+from matplotlib.colors import ListedColormap
+# ^^^ move to beginning when merging
+
+#plot parameters that I personally like, feel free to make these your own.
+matplotlib.rcParams['axes.facecolor'] = [0.9,0.9,0.9]
+matplotlib.rcParams['axes.labelsize'] = 14
+matplotlib.rcParams['axes.titlesize'] = 14
+matplotlib.rcParams['xtick.labelsize'] = 12
+matplotlib.rcParams['ytick.labelsize'] = 12
+matplotlib.rcParams['legend.fontsize'] = 12
+matplotlib.rcParams['legend.facecolor'] = 'w'
+matplotlib.rcParams['hatch.linewidth'] = 0.25
+
 class ClimateStripy:
   """ 
   Authors: Randy J. Chase, ENTER YOUR NAME HERE, ENTER YOUR NAME HERE
@@ -187,81 +205,214 @@ class ClimateStripy:
       ax.plot(self.station.longitude,self.station.latitude,'sk',ms=10,markerfacecolor='w',markeredgewidth=2,label='Closest_Station')
       ax.legend()
 
-  def draw_my_stripes(self,which='TMAX',method='Y',plot_val=True,savefig=False):
-    '''
-    This method takes the climate data time series and plot the stripes, optinally
-    adds a quantitative plot on top of the plot to show anomaly data, and optinally
-    saves the figure as a file.
+  def select_station(self,id=None):
+    """ 
+    In case you dont like the closest station, you can select a station from the 
+    returned station_df
+    """
+
+    if id is None: 
+      pass
+    else:
+      if self.station_df is not None:
+        temp_df = self.station_df.set_index("id")
+        self.station = temp_df.loc[id]
+        self.station_id = id
+      else:
+        self.station_id = id       
+        self.grab_station_info()
+
+  def grab_station_info(self):
+    print('Searching for station info. Please be patient.')
+    station_found_flag = True
+    it = 1
+    it2 = 0
+    while station_found_flag:
+      list_of_stations = (make_request('stations',payload={'limit':'1000','datasetid':'GHCND','offset':str(it),'sortorder':'desc'})).json()
+      df = pd.DataFrame(list_of_stations['results'])
+      if not df.where(df.id==self.station_id).dropna().empty:
+        station_found_flag = False
+        print('station found')
+        df = df.where(df.id==self.station_id).dropna()
+        df = df.astype({'mindate': np.datetime64})
+        df = df.astype({'maxdate': np.datetime64})
+        self.station = df.iloc[0,:]
+        self
+
+      it += 1000
+      it2 += 1
+
+      if (it2 > 139):
+        print('station not found. please check spelling')
+        break 
+
+  def fetch(self):
+    """ This method invokes the other method to grab data from the archive"""
+
+    years =  (self.station.maxdate - self.station.mindate).total_seconds()/3.154e+7
+    years = np.arange(0,np.round(years,0))
+    #to debug, let just do 10 years. 
+    # years = np.arange(0,10)
+
+    self.years = years
+    self.mindate = self.station.mindate
+    self.maxdate = self.station.maxdate
+    self.TMIN = None
+    self.TMAX = None
+    self.kill_flag = False
+    self.current = str(self.mindate)
+    from tqdm import tqdm
+    for i in tqdm(range(len(years))):
+        self.fetch_inside()
     
-    which: a 4-char string to specify data to plot. "TMIN" or "TMAX" supported now.
-    method: a 1-char flag to specify period to plot. "Y": annual; "M": monthly.
-      "W": weekly (not supported now and causes error)
-    plot_val: boolean to specify if quantatitive plot is shown.
-    savefig: boolean to specify if plot should be saved as a file.
-    '''
-    if method == 'Y':
-      if which=='TMAX':
-        anomaly = self.TMAX_YR_anom
+  def fetch_inside(self):
+    """ This method grabs the data for some year and some station"""
+    station_id = self.station.id
+    year = self.current[0:4]
+    #make the api call
+    r = requests.get('https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND&datatypeid=TMIN&datatypeid=TMAX&limit=1000&stationid='+station_id+'&startdate='+year+'-01-01&enddate='+year+'-12-31', headers={'token':self.token})
+    #if its empty go to the next year. 
+    if len(list(r)) <= 1:
+      pass
+    else:
+      df = pd.DataFrame(r.json()['results'])
+      tmax = df.where(df.datatype=='TMAX').dropna(how='all')
+      tmin = df.where(df.datatype=='TMIN').dropna(how='all')
+      if self.TMIN is None:
+        self.TMIN = tmin
+        self.TMAX = tmax 
       else:
-        anomaly = self.TMIN_YR_anom
-    elif method =='M':
-      if which=='TMAX':
-        anomaly = self.TMAX_MO_anom
-      else:
-        anomaly = self.TMIN_MO_anom
-    elif method =='W':
-      print('Your request could not be met, please choose another method')
+        self.TMIN = self.TMIN.append(tmin)
+        self.TMAX = self.TMAX.append(tmax)
 
-    date = anomaly.index
-    first = mdates.date2num(date[0])
-    last = mdates.date2num(date[-1])
-    width = (last - first) / (date.size-1)
-    [lower, upper] = [anomaly.min() - 0.1, anomaly.max() + 0.1]
+    self.current = str(np.asarray(self.current,dtype=np.datetime64) +np.timedelta64(365,'D'))
 
-    # initialize the plot
-    fig = plt.figure(figsize=(11, 1))
-    ax = fig.add_axes([0, 0, 1, 1])
-    # ax.set_axis_off()
+  def clean_df(self):
+    """ This just cleans up the data a bit """
+    series_tmin = pd.Series(self.TMIN.value.values/10, index=pd.DatetimeIndex(self.TMIN.date.astype(np.datetime64)))
+    series_tmax = pd.Series(self.TMAX.value.values/10, index=pd.DatetimeIndex(self.TMAX.date.astype(np.datetime64)))
 
-    # create a color map for stripes
-    cmap = ListedColormap([
-        '#08306b', '#08519c', '#2171b5', '#4292c6',
-        '#6baed6', '#9ecae1', '#c6dbef', '#deebf7',
-        '#fee0d2', '#fcbba1', '#fc9272', '#fb6a4a',
-        '#ef3b2c', '#cb181d', '#a50f15', '#67000d',])
+    self.TMIN = series_tmin
+    self.TMAX = series_tmax
 
+    idx = pd.date_range(str(self.station.mindate)[0:4]+'-01-01',str(self.station.maxdate)[0:4]+'-12-31')
+    self.TMIN = self.TMIN.reindex(idx, fill_value=np.nan)
+    self.TMAX = self.TMAX.reindex(idx, fill_value=np.nan)
 
-    patches = [Rectangle((date, lower), width, upper-lower) for date in np.arange(first, last + width,width)]
-    ind = np.where(~anomaly.isna())[0]
-    patches2 = []
-    for i in ind:
-      patches2.append(patches[i])
+  def test_data_avail(self):
+    """ Double check to see if there is enough data in the 1970 - 2000 time 
+    frame to calc anom """
+    chunk = self.TMIN[slice('1971-01-01','2000-12-31')].values.shape[0]
+    chunk = chunk/(30*365)
+    if chunk < 0.8:
+      print('ERROR: Data needed for anomaly calculation is too low (<80% there) Please select a different station from self.')
+    else:
+      print('Data are fine')
 
-    ind2 = np.where(anomaly.isna())[0]
-    patches3 = []
-    for i in ind2:
-      patches3.append(patches[i])
+  def get_monthly_anom(self):
+    count_tmin = self.TMIN.groupby(by=[CS.TMIN.index.year,CS.TMIN.index.month]).count()
+    count_tmax = self.TMAX.groupby(by=[CS.TMAX.index.year,CS.TMAX.index.month]).count()
 
-    # create a collection with a rectangle for each year
-    col = PatchCollection(patches2)
-    col2 = PatchCollection(patches3,hatch='x',facecolor=[0.75,0.75,0.75],edgecolor='k',lw=0.25)
-    # set data, colormap and color limits
-    col.set_array(anomaly[ind])
-    col.set_cmap(cmap)
-    col.set_clim([-2.6,2.6])
-    ax.add_collection(col)
-    ax.add_collection(col2)
+    #resample native daily to monthly 
+    self.TMIN_MO = self.TMIN.resample('MS').mean()
+    self.TMAX_MO = self.TMAX.resample('MS').mean()
+    #get baseline for anomaly calc. 
+    self.TMIN_mo = self.TMIN_MO[slice('1971-01-01','2000-12-31')]
+    TMIN_baseline =self.TMIN_mo.groupby(by=self.TMIN_mo.index.month).mean()
+    TMIN_std = self.TMIN_mo.groupby(by=self.TMIN_mo.index.month).std()
+    self.TMIN_MO_anom = self.TMIN_MO.groupby(by=self.TMIN_MO.index.year).apply(calc_anom(TMIN_baseline,TMIN_std))
+    self.TMIN_MO_anom = self.TMIN_MO_anom.where(count_tmin.values > (0.8*30)) #make sure 80% of the days are in that month to count 
 
-    ax.set_ylim(lower, upper)
-    ax.set_xlim(first, last + width)
+    self.TMAX_mo = self.TMAX_MO[slice('1971-01-01','2000-12-31')]
+    TMAX_baseline =self.TMAX_mo.groupby(by=self.TMAX_mo.index.month).mean()
+    TMAX_std = self.TMAX_mo.groupby(by=self.TMAX_mo.index.month).std()
+    self.TMAX_MO_anom = self.TMAX_MO.groupby(by=self.TMAX_MO.index.year).apply(calc_anom(TMAX_baseline,TMAX_std))
+    self.TMAX_MO_anom = self.TMAX_MO_anom.where(count_tmax.values > (0.8*30)) #make sure 80% of the days are in that month to count 
 
-    ax.set_title(self.station['name'])
-    if plot_val:
-        ax.plot(date, anomaly, markersize = 8, marker = 'o', linestyle = '--', markerfacecolor='w',color = 'k',path_effects=[path_effects.SimpleLineShadow(),
-                      path_effects.Normal()])
-    plt.show()
-    if savefig:
-        fig.savefig('climate-stripes.png')
+  def get_yearly_anom(self):
+    count_tmin = self.TMIN.groupby(by=[CS.TMIN.index.year]).count()
+    count_tmax = self.TMAX.groupby(by=[CS.TMAX.index.year]).count()
+    #resample native daily to monthly 
+    self.TMIN_YR = self.TMIN.resample('YS').mean()
+    self.TMAX_YR = self.TMAX.resample('YS').mean()
+    #get baseline for anomaly calc. 
+    self.TMIN_yr = self.TMIN_YR[slice('1971-01-01','2000-12-31')]
+    TMIN_baseline =self.TMIN_yr.groupby(by=self.TMIN_yr.index.month).mean()
+    TMIN_std = self.TMIN_yr.groupby(by=self.TMIN_yr.index.month).std()
+    self.TMIN_YR_anom = self.TMIN_YR.groupby(by=self.TMIN_YR.index.year).apply(calc_anom(TMIN_baseline,TMIN_std))
+    self.TMIN_YR_anom = self.TMIN_YR_anom.where(count_tmin.values > (0.8*365)) #make sure 80% of the days are in that month to count 
+
+    self.TMAX_yr = self.TMAX_YR[slice('1971-01-01','2000-12-31')]
+    TMAX_baseline =self.TMAX_yr.groupby(by=self.TMAX_yr.index.month).mean()
+    TMAX_std = self.TMAX_yr.groupby(by=self.TMAX_yr.index.month).std()
+    self.TMAX_YR_anom = self.TMAX_YR.groupby(by=self.TMAX_YR.index.year).apply(calc_anom(TMAX_baseline,TMAX_std))
+    self.TMAX_YR_anom = self.TMAX_YR_anom.where(count_tmax.values > (0.8*365)) #make sure 80% of the days are in that month to count 
+
+  def draw_my_stripes(self,which='TMAX',method='Y',plot_val=True,savefig=False):
+
+      if method == 'Y':
+        if which=='TMAX':
+          anomaly = self.TMAX_YR_anom
+        else:
+          anomaly = self.TMIN_YR_anom
+      elif method =='M':
+        if which=='TMAX':
+          anomaly = self.TMAX_MO_anom
+        else:
+          anomaly = self.TMIN_MO_anom
+      elif method =='W':
+        print('Your request could not be met, please choose another method')
+      
+      date = anomaly.index
+      first = mdates.date2num(date[0])
+      last = mdates.date2num(date[-1])
+      width = (last - first) / (date.size-1)
+      [lower, upper] = [anomaly.min() - 0.1, anomaly.max() + 0.1]
+
+      # initialize the plot
+      fig = plt.figure(figsize=(11, 1))
+      ax = fig.add_axes([0, 0, 1, 1])
+      # ax.set_axis_off()
+
+      # create a color map for stripes
+      cmap = ListedColormap([
+          '#08306b', '#08519c', '#2171b5', '#4292c6',
+          '#6baed6', '#9ecae1', '#c6dbef', '#deebf7',
+          '#fee0d2', '#fcbba1', '#fc9272', '#fb6a4a',
+          '#ef3b2c', '#cb181d', '#a50f15', '#67000d',])
+
+      
+      patches = [Rectangle((date, lower), width, upper-lower) for date in np.arange(first, last + width,width)]
+      ind = np.where(~anomaly.isna())[0]
+      patches2 = []
+      for i in ind:
+        patches2.append(patches[i])
+
+      ind2 = np.where(anomaly.isna())[0]
+      patches3 = []
+      for i in ind2:
+        patches3.append(patches[i])
+
+      # create a collection with a rectangle for each year
+      col = PatchCollection(patches2)
+      col2 = PatchCollection(patches3,hatch='x',facecolor=[0.75,0.75,0.75],edgecolor='k',lw=0.25)
+      # set data, colormap and color limits
+      col.set_array(anomaly[ind])
+      col.set_cmap(cmap)
+      col.set_clim([-2.6,2.6])
+      ax.add_collection(col)
+      ax.add_collection(col2)
+
+      ax.set_ylim(lower, upper)
+      ax.set_xlim(first, last + width)
+
+      ax.set_title(self.station['name'])
+      if plot_val:
+          ax.plot(date, anomaly, markersize = 8, marker = 'o', linestyle = '--', markerfacecolor='w',color = 'k',path_effects=[path_effects.SimpleLineShadow(),
+                        path_effects.Normal()])
+      plt.show()
+      if savefig:
+          fig.savefig('climate-stripes.png')
 
 #function not a method
 def calc_anom(baseline,std):
